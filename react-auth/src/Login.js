8b0578881b51d70408530dc58e523e00290a0499
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-04-11 15:11:22 -0500

    login:

--- react-auth/src/Login.js
+++ react-auth/src/Login.js
@@ -19,38 +19,42 @@ export default function Login() {
                 password
             }
         }
-            // make the API call
-    axios(configuration)
-    .then((result) => {console.log(result);})
-    .catch((error) => {console.log(error);})
+        // make the API call
+        axios(configuration)
+            .then((result) => {
+                setLogin(true);
+            })
+            .catch((error) => {
+                error = new Error();
+            });
 
     }
-    
+
     return (
         <>
             <h2>Login</h2>
-            <Form onSubmit={(e)=>handleSubmit(e)}>
+            <Form onSubmit={(e) => handleSubmit(e)}>
                 {/* email */}
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control 
-                    type="email" 
-                    placeholder="Enter email" 
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </Form.Group>
 
                 {/* password */}
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control 
-                    type="password" 
-                    placeholder="Password" 
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </Form.Group>
 
@@ -59,6 +63,12 @@ export default function Login() {
                     Submit
                 </Button>
             </Form>
+            {/* display success message */}
+            {login ? (
+                <p className="text-success">You Are Logged in Successfully</p>
+            ) : (
+                <p className="text-danger">You Are Not Logged in</p>
+            )}
 
         </>
     )
