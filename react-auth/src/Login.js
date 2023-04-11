880fc4355f0f05d954a9e46cb59d613e7bcb822a
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-04-11 15:00:25 -0500

    login

--- react-auth/src/Login.js
+++ react-auth/src/Login.js
@@ -1,21 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Button } from "react-bootstrap";
+import axios from "axios";
 
-export default function Register() {
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [login, setLogin] = useState(false);
+
+    const handleSubmit = (e) => {
+        // prevent the form from refreshing the whole page
+        e.preventDefault();
+        // set configutations
+        const configuration = {
+            method: "post",
+            url: "http://localhost:4000/login",
+            data: {
+                email,
+                password
+            }
+        }
+            // make the API call
+    axios(configuration)
+    .then((result) => {console.log(result);})
+    .catch((error) => {console.log(error);})
+
+    }
+    
     return (
         <>
             <h2>Login</h2>
-            <Form>
+            <Form onSubmit={(e)=>handleSubmit(e)}>
                 {/* email */}
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control 
+                    type="email" 
+                    placeholder="Enter email" 
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
                 </Form.Group>
 
                 {/* password */}
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control 
+                    type="password" 
+                    placeholder="Password" 
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
                 </Form.Group>
 
                 {/* submit button */}
