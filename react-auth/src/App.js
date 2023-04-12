0e31435b8d28d13a801ad3fa8dc65f918df784f8
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-04-12 09:26:57 -0500

    because all components under Routes need to be Route, we have to pass a render function and sando AuthComponent in a Protected Route component

--- react-auth/src/App.js
+++ react-auth/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container, Col, Row } from "react-bootstrap";
 import Register from "./Register";
 import Login from "./Login"
-import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Account from "./Account";
 import AuthComponent from "./AuthComponent";
 import FreeComponent from "./FreeComponent";
@@ -14,11 +14,19 @@ function App() {
       <Col className="text-center">
         <h1>React Authentication Tutorial</h1>
       </Col>
-        <Routes>
-          <Route exact path="/" element={<Account />} />
-          <Route exact path="/free" element={<FreeComponent />} />
-          <Route exact path="/auth" element={<AuthComponent />} />
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<Account />} />
+        <Route exact path="/free" element={<FreeComponent />} />
+        <Route
+          exact
+          path="/auth"
+          render={() => (
+            <ProtectedRoutes>
+              <AuthComponent />
+            </ProtectedRoutes>
+          )}
+        />
+      </Routes>
     </Container>
   );
 }
