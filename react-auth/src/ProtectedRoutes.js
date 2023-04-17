dda17c04db26d4ec8d61791656feed93f789f670
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2023-04-17 11:48:24 -0500

    auth is prtected

--- react-auth/src/ProtectedRoutes.js
+++ react-auth/src/ProtectedRoutes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, redirect , NavLink } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
@@ -7,20 +7,29 @@ const cookies = new Cookies();
 export default function ProtectedRoutes({ component: Component, ...rest }) {
   return (
 
-    // this route takes other routes assigned to it from the App.js and return the same route if condition is met
+    // this route takes other route assigned to it from the App.js and return the same route if condition is met
     <Route
       {...rest}
       render={(props) => {
         // get cookie from browser if logged in
         const token = cookies.get("TOKEN");
 
-        // returns route if there is a valid token set in the cookie
+        // return route if there is a valid token set in the cookie
         if (token) {
           return <Component {...props} />;
         } else {
-          // returns the user to the landing page if there is no valid token set
-          return <redirect to={{ pathname: "/", state: { from: props.location } }} />;
-
+          // return the user to the landing page if there is no valid token set
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: {
+                  // sets the location a user was about to assess before being redirected to login
+                  from: props.location,
+                },
+              }}
+            />
+          );
         }
       }}
     />
